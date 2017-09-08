d1f41a9b364d44d27a80782a6dba955c10410b54
juju4 <juju4@users.noreply.github.com>
2017-09-08 18:01:43 -0400

    fix pgdg play config + serverspec, switch to 9.6

--- test/integration/default-pgdg/serverspec/postgres_spec.rb
+++ test/integration/default-pgdg/serverspec/postgres_spec.rb
@@ -3,17 +3,24 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe package('postgresql-9.5'), :if => os[:family] == 'ubuntu' do
+describe package('postgresql-9.5'), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
   it { should be_installed }
 end
 describe package('postgresql95'), :if => os[:family] == 'redhat' do
   it { should be_installed }
 end
+describe package('postgresql95-server'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
 
-describe service('postgresql') do
+describe service('postgresql'), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
   it { should be_enabled   }
   it { should be_running   }
-end  
+end
+describe service('postgresql-9.5'), :if => os[:family] == 'redhat' do
+  it { should be_enabled   }
+  it { should be_running   }
+end
 
 describe process("postgres") do
   its(:user) { should eq "postgres" }
@@ -39,10 +46,10 @@ describe file('/usr/lib/postgresql/9.5/bin'), :if => os[:family] == 'ubuntu' do
 end
 
 
-describe file('/var/lib/pgsql'), :if => os[:family] == 'redhat' do
+describe file('/var/lib/pgsql/9.5'), :if => os[:family] == 'redhat' do
   it { should be_directory }
 end
-describe file('/var/lib/pgsql/data/pg_hba.conf'), :if => os[:family] == 'redhat' do
+describe file('/var/lib/pgsql/9.5/data/pg_hba.conf'), :if => os[:family] == 'redhat' do
   it { should be_file }
 end
 
